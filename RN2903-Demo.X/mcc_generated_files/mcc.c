a395a33201ea2352d6086efa52c076b9605669d1
Craig Peacock <craigp@beyondlogic.org>
2022-03-15 16:14:59 +1030

    Remove device_config.c and add configuration bits to mcc.c

--- RN2903-Demo.X/mcc_generated_files/mcc.c
+++ RN2903-Demo.X/mcc_generated_files/mcc.c
@@ -44,6 +44,69 @@
     SOFTWARE.
 */
 
+// Configuration bits: selected in the GUI
+
+// CONFIG1H
+#pragma config FOSC = INTIO67    // Oscillator Selection bits->Internal oscillator block
+#pragma config PLLCFG = OFF    // 4X PLL Enable->Oscillator used directly
+#pragma config PRICLKEN = OFF    // Primary clock enable bit->Primary clock can be disabled by software
+#pragma config FCMEN = OFF    // Fail-Safe Clock Monitor Enable bit->Fail-Safe Clock Monitor disabled
+#pragma config IESO = OFF    // Internal/External Oscillator Switchover bit->Oscillator Switchover mode disabled
+
+// CONFIG2L
+#pragma config PWRTEN = OFF    // Power-up Timer Enable bit->Power up timer disabled
+#pragma config BOREN = NOSLP    // Brown-out Reset Enable bits->Brown-out Reset enabled in hardware only and disabled in Sleep mode (SBOREN is disabled)
+#pragma config BORV = 190    // Brown Out Reset Voltage bits->VBOR set to 1.90 V nominal
+
+// CONFIG2H
+#pragma config WDTEN = OFF    // Watchdog Timer Enable bits->Watch dog timer is always disabled. SWDTEN has no effect.
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits->1:32768
+
+// CONFIG3H
+#pragma config CCP2MX = PORTC1    // CCP2 MUX bit->CCP2 input/output is multiplexed with RC1
+#pragma config PBADEN = ON    // PORTB A/D Enable bit->PORTB<5:0> pins are configured as analog input channels on Reset
+#pragma config CCP3MX = PORTB5    // P3A/CCP3 Mux bit->P3A/CCP3 input/output is multiplexed with RB5
+#pragma config HFOFST = ON    // HFINTOSC Fast Start-up->HFINTOSC output and ready status are not delayed by the oscillator stable status
+#pragma config T3CMX = PORTC0    // Timer3 Clock input mux bit->T3CKI is on RC0
+#pragma config P2BMX = PORTD2    // ECCP2 B output mux bit->P2B is on RD2
+#pragma config MCLRE = EXTMCLR    // MCLR Pin Enable bit->MCLR pin enabled, RE3 input pin disabled
+
+// CONFIG4L
+#pragma config STVREN = ON    // Stack Full/Underflow Reset Enable bit->Stack full/underflow will cause Reset
+#pragma config LVP = ON    // Single-Supply ICSP Enable bit->Single-Supply ICSP enabled if MCLRE is also 1
+#pragma config XINST = OFF    // Extended Instruction Set Enable bit->Instruction set extension and Indexed Addressing mode disabled (Legacy mode)
+#pragma config DEBUG = OFF    // Background Debug->Disabled
+
+// CONFIG5L
+#pragma config CP0 = ON    // Code Protection Block 0->Block 0 (000800-003FFFh) code-protected
+#pragma config CP1 = ON    // Code Protection Block 1->Block 1 (004000-007FFFh) code-protected
+#pragma config CP2 = ON    // Code Protection Block 2->Block 2 (008000-00BFFFh) code-protected
+#pragma config CP3 = ON    // Code Protection Block 3->Block 3 (00C000-00FFFFh) code-protected
+
+// CONFIG5H
+#pragma config CPB = ON    // Boot Block Code Protection bit->Boot block (000000-0007FFh) code-protected
+#pragma config CPD = ON    // Data EEPROM Code Protection bit->Data EEPROM code-protected
+
+// CONFIG6L
+#pragma config WRT0 = OFF    // Write Protection Block 0->Block 0 (000800-003FFFh) not write-protected
+#pragma config WRT1 = OFF    // Write Protection Block 1->Block 1 (004000-007FFFh) not write-protected
+#pragma config WRT2 = OFF    // Write Protection Block 2->Block 2 (008000-00BFFFh) not write-protected
+#pragma config WRT3 = OFF    // Write Protection Block 3->Block 3 (00C000-00FFFFh) not write-protected
+
+// CONFIG6H
+#pragma config WRTC = OFF    // Configuration Register Write Protection bit->Configuration registers (300000-3000FFh) not write-protected
+#pragma config WRTB = OFF    // Boot Block Write Protection bit->Boot Block (000000-0007FFh) not write-protected
+#pragma config WRTD = OFF    // Data EEPROM Write Protection bit->Data EEPROM not write-protected
+
+// CONFIG7L
+#pragma config EBTR0 = OFF    // Table Read Protection Block 0->Block 0 (000800-003FFFh) not protected from table reads executed in other blocks
+#pragma config EBTR1 = OFF    // Table Read Protection Block 1->Block 1 (004000-007FFFh) not protected from table reads executed in other blocks
+#pragma config EBTR2 = OFF    // Table Read Protection Block 2->Block 2 (008000-00BFFFh) not protected from table reads executed in other blocks
+#pragma config EBTR3 = OFF    // Table Read Protection Block 3->Block 3 (00C000-00FFFFh) not protected from table reads executed in other blocks
+
+// CONFIG7H
+#pragma config EBTRB = OFF    // Boot Block Table Read Protection bit->Boot Block (000000-0007FFh) not protected from table reads executed in other blocks
+
 #include "mcc.h"
 
 
--- RN2903-Demo.X/nbproject/Makefile-default.mk
+++ RN2903-Demo.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=mcc_generated_files/mcc.c mcc_generated_files/device_config.c mcc_generated_files/spi2.c mcc_generated_files/tmr1.c mcc_generated_files/LoRaWAN/AES.c mcc_generated_files/LoRaWAN/interrupt_manager_lora_addons.c mcc_generated_files/LoRaWAN/lorawan.c mcc_generated_files/LoRaWAN/lorawan_aes.c mcc_generated_files/LoRaWAN/lorawan_aes_cmac.c mcc_generated_files/LoRaWAN/radio_driver_hal.c mcc_generated_files/LoRaWAN/sw_timer.c mcc_generated_files/LoRaWAN/tmr_lora_addons.c mcc_generated_files/LoRaWAN/lorawan_init.c mcc_generated_files/LoRaWAN/lorawan_na.c mcc_generated_files/LoRaWAN/radio_driver_SX1276.c mcc_generated_files/pin_manager.c mcc_generated_files/ext_int.c mcc_generated_files/interrupt_manager.c mcc_generated_files/eusart1.c main.c mcc_generated_files/tmr3.c
+SOURCEFILES_QUOTED_IF_SPACED=mcc_generated_files/mcc.c mcc_generated_files/spi2.c mcc_generated_files/tmr1.c mcc_generated_files/LoRaWAN/AES.c mcc_generated_files/LoRaWAN/interrupt_manager_lora_addons.c mcc_generated_files/LoRaWAN/lorawan.c mcc_generated_files/LoRaWAN/lorawan_aes.c mcc_generated_files/LoRaWAN/lorawan_aes_cmac.c mcc_generated_files/LoRaWAN/radio_driver_hal.c mcc_generated_files/LoRaWAN/sw_timer.c mcc_generated_files/LoRaWAN/tmr_lora_addons.c mcc_generated_files/LoRaWAN/lorawan_init.c mcc_generated_files/LoRaWAN/lorawan_na.c mcc_generated_files/LoRaWAN/radio_driver_SX1276.c mcc_generated_files/pin_manager.c mcc_generated_files/ext_int.c mcc_generated_files/interrupt_manager.c mcc_generated_files/eusart1.c mcc_generated_files/tmr3.c main.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/mcc_generated_files/mcc.p1 ${OBJECTDIR}/mcc_generated_files/device_config.p1 ${OBJECTDIR}/mcc_generated_files/spi2.p1 ${OBJECTDIR}/mcc_generated_files/tmr1.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/AES.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/interrupt_manager_lora_addons.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes_cmac.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_hal.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/sw_timer.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/tmr_lora_addons.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_init.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_na.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_SX1276.p1 ${OBJECTDIR}/mcc_generated_files/pin_manager.p1 ${OBJECTDIR}/mcc_generated_files/ext_int.p1 ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1 ${OBJECTDIR}/mcc_generated_files/eusart1.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/mcc_generated_files/tmr3.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/mcc_generated_files/mcc.p1.d ${OBJECTDIR}/mcc_generated_files/device_config.p1.d ${OBJECTDIR}/mcc_generated_files/spi2.p1.d ${OBJECTDIR}/mcc_generated_files/tmr1.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/AES.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/interrupt_manager_lora_addons.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes_cmac.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_hal.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/sw_timer.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/tmr_lora_addons.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_init.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_na.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_SX1276.p1.d ${OBJECTDIR}/mcc_generated_files/pin_manager.p1.d ${OBJECTDIR}/mcc_generated_files/ext_int.p1.d ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1.d ${OBJECTDIR}/mcc_generated_files/eusart1.p1.d ${OBJECTDIR}/main.p1.d ${OBJECTDIR}/mcc_generated_files/tmr3.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/mcc_generated_files/mcc.p1 ${OBJECTDIR}/mcc_generated_files/spi2.p1 ${OBJECTDIR}/mcc_generated_files/tmr1.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/AES.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/interrupt_manager_lora_addons.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes_cmac.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_hal.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/sw_timer.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/tmr_lora_addons.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_init.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_na.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_SX1276.p1 ${OBJECTDIR}/mcc_generated_files/pin_manager.p1 ${OBJECTDIR}/mcc_generated_files/ext_int.p1 ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1 ${OBJECTDIR}/mcc_generated_files/eusart1.p1 ${OBJECTDIR}/mcc_generated_files/tmr3.p1 ${OBJECTDIR}/main.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/mcc_generated_files/mcc.p1.d ${OBJECTDIR}/mcc_generated_files/spi2.p1.d ${OBJECTDIR}/mcc_generated_files/tmr1.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/AES.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/interrupt_manager_lora_addons.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes_cmac.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_hal.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/sw_timer.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/tmr_lora_addons.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_init.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_na.p1.d ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_SX1276.p1.d ${OBJECTDIR}/mcc_generated_files/pin_manager.p1.d ${OBJECTDIR}/mcc_generated_files/ext_int.p1.d ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1.d ${OBJECTDIR}/mcc_generated_files/eusart1.p1.d ${OBJECTDIR}/mcc_generated_files/tmr3.p1.d ${OBJECTDIR}/main.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/mcc_generated_files/mcc.p1 ${OBJECTDIR}/mcc_generated_files/device_config.p1 ${OBJECTDIR}/mcc_generated_files/spi2.p1 ${OBJECTDIR}/mcc_generated_files/tmr1.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/AES.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/interrupt_manager_lora_addons.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes_cmac.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_hal.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/sw_timer.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/tmr_lora_addons.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_init.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_na.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_SX1276.p1 ${OBJECTDIR}/mcc_generated_files/pin_manager.p1 ${OBJECTDIR}/mcc_generated_files/ext_int.p1 ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1 ${OBJECTDIR}/mcc_generated_files/eusart1.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/mcc_generated_files/tmr3.p1
+OBJECTFILES=${OBJECTDIR}/mcc_generated_files/mcc.p1 ${OBJECTDIR}/mcc_generated_files/spi2.p1 ${OBJECTDIR}/mcc_generated_files/tmr1.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/AES.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/interrupt_manager_lora_addons.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_aes_cmac.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_hal.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/sw_timer.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/tmr_lora_addons.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_init.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/lorawan_na.p1 ${OBJECTDIR}/mcc_generated_files/LoRaWAN/radio_driver_SX1276.p1 ${OBJECTDIR}/mcc_generated_files/pin_manager.p1 ${OBJECTDIR}/mcc_generated_files/ext_int.p1 ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1 ${OBJECTDIR}/mcc_generated_files/eusart1.p1 ${OBJECTDIR}/mcc_generated_files/tmr3.p1 ${OBJECTDIR}/main.p1
 
 # Source Files
-SOURCEFILES=mcc_generated_files/mcc.c mcc_generated_files/device_config.c mcc_generated_files/spi2.c mcc_generated_files/tmr1.c mcc_generated_files/LoRaWAN/AES.c mcc_generated_files/LoRaWAN/interrupt_manager_lora_addons.c mcc_generated_files/LoRaWAN/lorawan.c mcc_generated_files/LoRaWAN/lorawan_aes.c mcc_generated_files/LoRaWAN/lorawan_aes_cmac.c mcc_generated_files/LoRaWAN/radio_driver_hal.c mcc_generated_files/LoRaWAN/sw_timer.c mcc_generated_files/LoRaWAN/tmr_lora_addons.c mcc_generated_files/LoRaWAN/lorawan_init.c mcc_generated_files/LoRaWAN/lorawan_na.c mcc_generated_files/LoRaWAN/radio_driver_SX1276.c mcc_generated_files/pin_manager.c mcc_generated_files/ext_int.c mcc_generated_files/interrupt_manager.c mcc_generated_files/eusart1.c main.c mcc_generated_files/tmr3.c
+SOURCEFILES=mcc_generated_files/mcc.c mcc_generated_files/spi2.c mcc_generated_files/tmr1.c mcc_generated_files/LoRaWAN/AES.c mcc_generated_files/LoRaWAN/interrupt_manager_lora_addons.c mcc_generated_files/LoRaWAN/lorawan.c mcc_generated_files/LoRaWAN/lorawan_aes.c mcc_generated_files/LoRaWAN/lorawan_aes_cmac.c mcc_generated_files/LoRaWAN/radio_driver_hal.c mcc_generated_files/LoRaWAN/sw_timer.c mcc_generated_files/LoRaWAN/tmr_lora_addons.c mcc_generated_files/LoRaWAN/lorawan_init.c mcc_generated_files/LoRaWAN/lorawan_na.c mcc_generated_files/LoRaWAN/radio_driver_SX1276.c mcc_generated_files/pin_manager.c mcc_generated_files/ext_int.c mcc_generated_files/interrupt_manager.c mcc_generated_files/eusart1.c mcc_generated_files/tmr3.c main.c
 
 
 
@@ -101,14 +101,6 @@ ${OBJECTDIR}/mcc_generated_files/mcc.p1: mcc_generated_files/mcc.c  nbproject/Ma
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=icd3   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/mcc_generated_files/mcc.p1 mcc_generated_files/mcc.c 
 	@-${MV} ${OBJECTDIR}/mcc_generated_files/mcc.d ${OBJECTDIR}/mcc_generated_files/mcc.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/mcc_generated_files/mcc.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/mcc_generated_files/device_config.p1: mcc_generated_files/device_config.c  nbproject/Makefile-${CND_CONF}.mk 
-	@${MKDIR} "${OBJECTDIR}/mcc_generated_files" 
-	@${RM} ${OBJECTDIR}/mcc_generated_files/device_config.p1.d 
-	@${RM} ${OBJECTDIR}/mcc_generated_files/device_config.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=icd3   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/mcc_generated_files/device_config.p1 mcc_generated_files/device_config.c 
-	@-${MV} ${OBJECTDIR}/mcc_generated_files/device_config.d ${OBJECTDIR}/mcc_generated_files/device_config.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/mcc_generated_files/device_config.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 ${OBJECTDIR}/mcc_generated_files/spi2.p1: mcc_generated_files/spi2.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}/mcc_generated_files" 
@@ -246,14 +238,6 @@ ${OBJECTDIR}/mcc_generated_files/eusart1.p1: mcc_generated_files/eusart1.c  nbpr
 	@-${MV} ${OBJECTDIR}/mcc_generated_files/eusart1.d ${OBJECTDIR}/mcc_generated_files/eusart1.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/mcc_generated_files/eusart1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk 
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/main.p1.d 
-	@${RM} ${OBJECTDIR}/main.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=icd3   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/main.p1 main.c 
-	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
 ${OBJECTDIR}/mcc_generated_files/tmr3.p1: mcc_generated_files/tmr3.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}/mcc_generated_files" 
 	@${RM} ${OBJECTDIR}/mcc_generated_files/tmr3.p1.d 
@@ -261,6 +245,14 @@ ${OBJECTDIR}/mcc_generated_files/tmr3.p1: mcc_generated_files/tmr3.c  nbproject/
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=icd3   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/mcc_generated_files/tmr3.p1 mcc_generated_files/tmr3.c 
 	@-${MV} ${OBJECTDIR}/mcc_generated_files/tmr3.d ${OBJECTDIR}/mcc_generated_files/tmr3.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/mcc_generated_files/tmr3.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.p1.d 
+	@${RM} ${OBJECTDIR}/main.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=icd3   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/main.p1 main.c 
+	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
 ${OBJECTDIR}/mcc_generated_files/mcc.p1: mcc_generated_files/mcc.c  nbproject/Makefile-${CND_CONF}.mk 
@@ -270,14 +262,6 @@ ${OBJECTDIR}/mcc_generated_files/mcc.p1: mcc_generated_files/mcc.c  nbproject/Ma
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/mcc_generated_files/mcc.p1 mcc_generated_files/mcc.c 
 	@-${MV} ${OBJECTDIR}/mcc_generated_files/mcc.d ${OBJECTDIR}/mcc_generated_files/mcc.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/mcc_generated_files/mcc.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/mcc_generated_files/device_config.p1: mcc_generated_files/device_config.c  nbproject/Makefile-${CND_CONF}.mk 
-	@${MKDIR} "${OBJECTDIR}/mcc_generated_files" 
-	@${RM} ${OBJECTDIR}/mcc_generated_files/device_config.p1.d 
-	@${RM} ${OBJECTDIR}/mcc_generated_files/device_config.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/mcc_generated_files/device_config.p1 mcc_generated_files/device_config.c 
-	@-${MV} ${OBJECTDIR}/mcc_generated_files/device_config.d ${OBJECTDIR}/mcc_generated_files/device_config.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/mcc_generated_files/device_config.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 ${OBJECTDIR}/mcc_generated_files/spi2.p1: mcc_generated_files/spi2.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}/mcc_generated_files" 
@@ -415,14 +399,6 @@ ${OBJECTDIR}/mcc_generated_files/eusart1.p1: mcc_generated_files/eusart1.c  nbpr
 	@-${MV} ${OBJECTDIR}/mcc_generated_files/eusart1.d ${OBJECTDIR}/mcc_generated_files/eusart1.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/mcc_generated_files/eusart1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk 
-	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/main.p1.d 
-	@${RM} ${OBJECTDIR}/main.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/main.p1 main.c 
-	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
 ${OBJECTDIR}/mcc_generated_files/tmr3.p1: mcc_generated_files/tmr3.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}/mcc_generated_files" 
 	@${RM} ${OBJECTDIR}/mcc_generated_files/tmr3.p1.d 
@@ -430,6 +406,14 @@ ${OBJECTDIR}/mcc_generated_files/tmr3.p1: mcc_generated_files/tmr3.c  nbproject/
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/mcc_generated_files/tmr3.p1 mcc_generated_files/tmr3.c 
 	@-${MV} ${OBJECTDIR}/mcc_generated_files/tmr3.d ${OBJECTDIR}/mcc_generated_files/tmr3.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/mcc_generated_files/tmr3.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.p1.d 
+	@${RM} ${OBJECTDIR}/main.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/main.p1 main.c 
+	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
