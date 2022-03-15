c9db263b3e5e1e1ffdbbec67a22ec05ed1407f04
Craig Peacock <craigp@beyondlogic.org>
2022-03-15 16:16:38 +1030

    Updated from codebase running on RN2903 SA AU915 v1.0.3

--- RN2903-Demo.X/mcc_generated_files/tmr1.c
+++ RN2903-Demo.X/mcc_generated_files/tmr1.c
@@ -8,40 +8,39 @@
     tmr1.c
 
   @Summary
-    This is the generated driver implementation file for the TMR1 driver using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+    This is the generated driver implementation file for the TMR1 driver using MPLAB(c) Code Configurator
 
   @Description
     This source file provides APIs for TMR1.
     Generation Information :
-        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.65.2
+        Product Revision  :  MPLAB(c) Code Configurator - 4.0
         Device            :  PIC18LF46K22
-        Driver Version    :  2.01
+        Driver Version    :  2.00
     The generated drivers are tested against the following:
-        Compiler          :  XC8 1.45
-        MPLAB 	          :  MPLAB X 4.15
+        Compiler          :  XC8 1.35
+        MPLAB             :  MPLAB X 3.40
 */
 
 /*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
-    Subject to your compliance with these terms, you may use Microchip software and any 
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
-    license terms applicable to your use of third party software (including open source software) that 
-    may accompany Microchip software.
-    
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
-    FOR A PARTICULAR PURPOSE.
-    
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
-    SOFTWARE.
+    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
 */
 
 /**
@@ -56,7 +55,6 @@
 */
 volatile uint16_t timer1ReloadVal;
 void (*TMR1_InterruptHandler)(void);
-
 /**
   Section: TMR1 APIs
 */
@@ -65,7 +63,10 @@ void TMR1_Initialize(void)
 {
     //Set the Timer to the options selected in the GUI
 
-    //T1GSS T1G_pin; TMR1GE disabled; T1GTM disabled; T1GPOL low; T1GGO done; T1GSPM disabled; 
+    //T1CKPS 1:1; T1OSCEN enabled; T1SYNC do_not_synchronize; TMR1CS External; TMR1ON off; T1RD16 disabled; 
+    T1CON = 0x8C;
+
+    //T1GSS T1G; TMR1GE disabled; T1GTM disabled; T1GPOL low; T1GGO done; T1GSPM disabled; 
     T1GCON = 0x00;
 
     //TMR1H 0; 
@@ -86,8 +87,8 @@ void TMR1_Initialize(void)
     // Set Default Interrupt Handler
     TMR1_SetInterruptHandler(TMR1_DefaultInterruptHandler);
 
-    // T1CKPS 1:1; T1OSCEN enabled; T1SYNC do_not_synchronize; TMR1CS External; TMR1ON enabled; T1RD16 disabled; 
-    T1CON = 0x8D;
+    // Start TMR1
+    TMR1_StartTimer();
 }
 
 void TMR1_StartTimer(void)
@@ -108,8 +109,6 @@ uint16_t TMR1_ReadTimer(void)
     uint8_t readValHigh;
     uint8_t readValLow;
     
-    T1CONbits.T1RD16 = 1;
-	
     readValLow = TMR1L;
     readValHigh = TMR1H;
     
@@ -142,7 +141,9 @@ void TMR1_WriteTimer(uint16_t timerVal)
 
 void TMR1_Reload(void)
 {
-    TMR1_WriteTimer(timer1ReloadVal);
+    // Write to the Timer1 register
+    TMR1H = (timer1ReloadVal >> 8);
+    TMR1L = (uint8_t) timer1ReloadVal;
 }
 
 void TMR1_StartSinglePulseAcquisition(void)
@@ -160,7 +161,10 @@ void TMR1_ISR(void)
 
     // Clear the TMR1 interrupt flag
     PIR1bits.TMR1IF = 0;
-    TMR1_WriteTimer(timer1ReloadVal);
+    
+    // Write to the Timer1 register
+    TMR1H = (timer1ReloadVal >> 8);
+    TMR1L = (uint8_t) timer1ReloadVal;
 
     // ticker function call;
     // ticker is 1 -> Callback function gets called everytime this ISR executes
@@ -176,7 +180,7 @@ void TMR1_CallBack(void)
     }
 }
 
-void TMR1_SetInterruptHandler(void (* InterruptHandler)(void)){
+void TMR1_SetInterruptHandler(void* InterruptHandler){
     TMR1_InterruptHandler = InterruptHandler;
 }
 
--- RN2903-Demo.X/mcc_generated_files/tmr1.h
+++ RN2903-Demo.X/mcc_generated_files/tmr1.h
@@ -8,44 +8,43 @@
     tmr1.h
 
   @Summary
-    This is the generated header file for the TMR1 driver using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+    This is the generated header file for the TMR1 driver using MPLAB(c) Code Configurator
 
   @Description
     This header file provides APIs for driver for TMR1.
     Generation Information :
-        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.65.2
+        Product Revision  :  MPLAB(c) Code Configurator - 4.0
         Device            :  PIC18LF46K22
-        Driver Version    :  2.01
+        Driver Version    :  2.00
     The generated drivers are tested against the following:
-        Compiler          :  XC8 1.45
-        MPLAB 	          :  MPLAB X 4.15
+        Compiler          :  XC8 1.35
+        MPLAB             :  MPLAB X 3.40
 */
 
 /*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
-    Subject to your compliance with these terms, you may use Microchip software and any 
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
-    license terms applicable to your use of third party software (including open source software) that 
-    may accompany Microchip software.
-    
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
-    FOR A PARTICULAR PURPOSE.
-    
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
-    SOFTWARE.
+    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
 */
 
-#ifndef TMR1_H
-#define TMR1_H
+#ifndef _TMR1_H
+#define _TMR1_H
 
 /**
   Section: Included Files
@@ -380,7 +379,7 @@ void TMR1_CallBack(void);
   @Returns
     None
 */
- void TMR1_SetInterruptHandler(void (* InterruptHandler)(void));
+ void TMR1_SetInterruptHandler(void *InterruptHandler);
 
 /**
   @Summary
@@ -425,7 +424,7 @@ void TMR1_DefaultInterruptHandler(void);
 
 #endif
 
-#endif // TMR1_H
+#endif // _TMR1_H
 /**
  End of File
 */
